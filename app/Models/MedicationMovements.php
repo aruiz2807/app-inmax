310411c8188aa5ece4f715db44f5e23483efa642
Fernando Ruvira <ferruvher@gmail.com>
2026-06-05 12:19:11 -0600

    feat: se añade modelo y migracion de medication_movements

--- app/Models/MedicationMovements.php
+++ app/Models/MedicationMovements.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class MedicationMovements extends Model
 {
-    //
+    protected $fillable = [
+        'medication_id',
+        'type',
+        'adjustment',
+        'quantity',
+        'reference',
+        'prescription_id',
+        'medication_purchase_id',
+        'user_id',
+    ];
+
+    public function medication()
+    {
+        return $this->belongsTo(Medication::class);
+    }
+
+    public function prescription()
+    {
+        return $this->belongsTo(Prescription::class);
+    }
+
+    public function medicationPurchase()
+    {
+        return $this->belongsTo(MedicationPurchase::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
