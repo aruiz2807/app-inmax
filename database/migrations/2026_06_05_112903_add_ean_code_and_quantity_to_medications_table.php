37bc5de6f9764b99dafd9d7e36cb163f49b5ca03
Fernando Ruvira <ferruvher@gmail.com>
2026-06-05 11:31:58 -0600

    feat: migracion de ean_code y quantity en tabla medications

---
+++ database/migrations/2026_06_05_112903_add_ean_code_and_quantity_to_medications_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('medications', function (Blueprint $table) {
+            $table->text('ean_code')->after('code');
+            $table->integer('quantity')->default(0)->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('medications', function (Blueprint $table) {
+            $table->dropColumn('ean_code');
+            $table->dropColumn('quantity');
+        });
+    }
+};
