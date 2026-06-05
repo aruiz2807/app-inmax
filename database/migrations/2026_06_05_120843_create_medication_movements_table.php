364e1622bad217aa93f6a4b4c6219e55de133ac5
Fernando Ruvira <ferruvher@gmail.com>
2026-06-05 12:18:53 -0600

    feat: se añade modelo y migracion de medication_movements

---
+++ database/migrations/2026_06_05_120843_create_medication_movements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medication_movements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('medication_id')->constrained()->cascadeOnDelete();
+            $table->enum('type', ['IN', 'OUT'])->default('IN');
+            $table->boolean('adjustment')->default(false);
+            $table->integer('quantity')->default(0);
+            $table->text('reference')->nullable();
+            $table->foreignId('prescription_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('medication_purchase_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medication_movements');
+    }
+};
